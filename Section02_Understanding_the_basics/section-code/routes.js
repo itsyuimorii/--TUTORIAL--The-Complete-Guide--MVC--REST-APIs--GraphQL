330ec6cd6f3e292313ed0a7151d1da566477a4a4
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-09 19:47:12 -0500

    section02 update

--- Section02_Understanding_the_basics/section-code/routes.js
+++ Section02_Understanding_the_basics/section-code/routes.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 
 const requestHandler = (req, res) => {
-
+  
   const url = req.url
   const method = req.method
+
+
   if (url === '/') {
     res.write('<html>')
     res.write('<head><title>Enter Message</title></head>')
@@ -15,19 +17,9 @@ const requestHandler = (req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', chunk => {
-      console.log(chunk)
-      body.push(chunk)
-    })
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
-      })
+ 
+    fs.writeFile('message.text', 'Dummy',err=>{
+      res. statusCode = 302;
     })
   }
   res.setHeader('Content-Type', 'text/html')
